4b6d7974c3046d60bae70fe8412e7f4f5562b2cd
Kevin Mårtensson <kevinmartensson@gmail.com>
2016-09-06 20:41:01 +0200

    Add test for deflated content (#5)

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ import rfpify from 'rfpify';
 import m from '../';
 import {createServer} from './helpers/server.js';
 
-const gzipP = pify(zlib.gzip);
+const zlibP = pify(zlib);
 const httpGetP = rfpify(http.get);
 const fixture = 'Compressible response content.\n';
 
@@ -20,21 +20,39 @@ test.before('setup', async () => {
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/plain');
 		res.setHeader('Content-Encoding', 'gzip');
-		res.end(await gzipP(fixture));
+		res.end(await zlibP.gzip(fixture));
+	});
+
+	s.on('/deflate', async (req, res) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Encodingnt-Type', 'text/plain');
+		res.setHeader('Content-Encoding', 'deflate');
+		res.end(await zlibP.deflate(fixture));
 	});
 
 	s.on('/missing-data', async (req, res) => {
 		res.statusCode = 200;
 		res.setHeader('Content-Encodingnt-Type', 'text/plain');
 		res.setHeader('Content-Encoding', 'gzip');
-		res.end((await gzipP(fixture)).slice(0, -1));
+		res.end((await zlibP.gzip(fixture)).slice(0, -1));
 	});
 
 	await s.listen(s.port);
 });
 
-test('unzip content', async t => {
+test('unzip gzipped content', async t => {
 	const res = m(await httpGetP(s.url));
+
+	t.is(typeof res.httpVersion, 'string');
+	t.truthy(res.headers);
+
+	res.setEncoding('utf8');
+
+	t.is(await getStream(res), fixture);
+});
+
+test('unzip deflated content', async t => {
+	const res = m(await httpGetP(`${s.url}/deflate`));
 
 	t.is(typeof res.httpVersion, 'string');
 	t.truthy(res.headers);
