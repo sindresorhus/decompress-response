cc2e67d9b96d4b2724b786234180f9eeea084366
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-11 13:44:36 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,10 @@
 /// <reference types="node"/>
-import * as http from 'http';
+import {IncomingMessage} from 'http';
 
 /**
  * Decompress a HTTP response if needed.
  *
- * @param response The HTTP incoming stream with compressed data.
+ * @param response - The HTTP incoming stream with compressed data.
  * @returns The decompressed HTTP response stream.
  */
-export default function decompressResponse(
-	response: http.IncomingMessage
-): http.IncomingMessage;
+export default function decompressResponse(response: IncomingMessage): IncomingMessage;
