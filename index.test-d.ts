a71ee071b728058f4c784d72b8b0f629c74c0654
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-11 13:44:59 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,6 @@
+import * as http from 'http';
 import {expectType} from 'tsd-check';
 import decompressResponse from '.';
-import * as http from 'http';
 
 http.get('localhost', response => {
 	expectType<http.IncomingMessage>(decompressResponse(response));
